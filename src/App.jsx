fe6f295d89cae70b2c137acd425790f3425d89fb
Juan Manuel Gonzalez <111544301+Juanmags97@users.noreply.github.com>
2023-10-11 12:08:06 -0300

    juanma

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import logo from './assets/logo-01.png'
+import { useState } from "react";
+import "./App.css";
+import logo from "./assets/logo-01.png";
+import icono from "./assets/logo-hover.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <h1> Aprender es evolucionar</h1>
-        <div className="logo"> 
-        <img src={logo} />
         </div>
-        
-      </div>
-    
+        <div className="logos">
+          <img className="logo" src={logo} />
+          <img  className="icono" src={icono}  />
+        </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
