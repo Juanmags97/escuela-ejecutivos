1813a9025cc0178b97c502153f0785b3804cf3ae
Juan Manuel Gonzalez <111544301+Juanmags97@users.noreply.github.com>
2023-10-11 10:39:40 -0300

    juanma

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>hola mundo</h1>
+        <header>
+          
+        </header>
       </div>
     
     </>
