6d22d9e5417e8d3af6f8bd238ca934d53f88d8b5
Juan Manuel Gonzalez <111544301+Juanmags97@users.noreply.github.com>
2023-10-12 17:54:54 -0300

    juanma

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
 import { useState } from "react";
 import { Routes, Route,} from 'react-router-dom'
 import "./App.css";
-import icono from "./assets/logo-hover.png"
 
 
 function App() {
 
   return (
     <>
-      <div className="logos">
-      
-        <img className="icono" src={icono} />
-      </div>
-
-      <form className="formulario" action="" method="get">
-          <input type="text" placeholder="Correo" />
-        <input type="password" placeholder="Contraseña" />
-
-        <button type="button" className="btnLogin">Ingresar</button>
-
-      </form>
-
-      <div>
-        <span className="frase"> Aprender es evolucionars</span>
-      
-      </div>
+    <Login/>
 
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,26 @@
-puto el que lea
+import React from "react";
+import icono from "../assets/logo-hover.png";
+
+
+export const Login = () => {
+    return (
+    <div>
+        <div className="logos">
+            <img className="icono" src={icono} />
+        </div>
+
+        <form className="formulario" action="" method="get">
+            <input type="text" placeholder="Correo" />
+            <input type="password" placeholder="Contraseña" />
+
+        <button type="button" className="btnLogin">
+            Ingresar
+        </button>
+        </form>
+
+        <div>
+            <span className="frase"> Aprender es evolucionar</span>
+        </div>
+    </div>
+    );
+};
