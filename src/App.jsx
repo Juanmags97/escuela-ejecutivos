b940bd67cf456aefc4f5311efa3408d8f249267c
Juan Manuel Gonzalez <111544301+Juanmags97@users.noreply.github.com>
2023-10-12 18:16:33 -0300

    juanma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Routes, Route,} from 'react-router-dom'
+import { Login } from './components/Login'
+import {Loading} from './components/Loading'
 import "./App.css";
 
 
@@ -8,6 +10,10 @@ function App() {
   return (
     <>
     <Login/>
+    <Routes>
+    <Route path="/" element={<Loading />}/>
+    </Routes>
+    
 
     </>
   );
