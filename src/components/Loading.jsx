337bbe57282936b1438173f06c820a8dab00356f
rater31 <85116780+rater31@users.noreply.github.com>
2023-10-14 12:22:41 -0300

    fede
    tenes que hacer un npm i

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Letras from "../assets/escuela-de-ejecutivos.png"
-import icono from "../assets/logo-hover.png";
+import icono from "../assets/logo-hover.png"
+// import { motion, useInView } from "framer-motion"
+
 const Loading = () => {
+
+
+
     return (
-        <div>
-            <h1>Hola mundo infeliz</h1>
-            <img src={icono} alt="" />
-            <img src={Letras} alt="" />
+        <div className='cargando'>
+            {/* <h1>Hola mundo infeliz</h1> */}
+            <img className='icono-loading' src={icono} alt="" />
+            <img className='icono-loading-letra' src={Letras} alt="" />
+
+            
         </div>
     );
 }
