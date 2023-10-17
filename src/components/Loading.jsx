c8cf3ef29da2b6fd06901be5984a1abeb02b587d
rater31 <fedehidalgo89@gmail.com>
2023-10-17 19:21:24 -0300

    fede

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import {React, useRef} from 'react';
 import Letras from "../assets/escuela-de-ejecutivos.png"
 import icono from "../assets/logo-hover.png"
-import { motion, useInView } from "framer-motion
+import { motion, useInView } from "framer-motion"
 
 const Loading = () => {
-
-
-
+    function IzDer({ children }) {
+        const ref = useRef(null);
+        const isInView = useInView(ref, { once: true });
+  
+        return (
+           <section ref={ref}>
+              <span
+                 style={{
+                    transform: isInView ? "none" : "translateX(-100px)",
+                    opacity: isInView ? 1 : 0,
+                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                 }}
+              >
+                 {children}
+              </span>
+           </section>
+        );
+     }
     return (
         <div className='cargando'>
             {/* asdasdasdasdasd<h1>Hola mundo infeliz</h1> */}
             <img className='icono-loading' src={icono} alt="" />
-            <img className='icono-loading-letra' src={Letras} alt="" />
+            <IzDer><img className='icono-loading-letra' src={Letras} alt="" /></IzDer>
             
         </div>
     );
