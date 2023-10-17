a6a8bbef1e4097daccf022623aac175e893662e3
Juan Manuel Gonzalez <111544301+Juanmags97@users.noreply.github.com>
2023-10-17 18:59:11 -0300

    juanma

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Letras from "../assets/escuela-de-ejecutivos.png"
 import icono from "../assets/logo-hover.png"
-// import { motion, useInView } from "framer-motion"
+import { motion, useInView } from "framer-motion
 
 const Loading = () => {
 
