cd8c48ab78996ecb1ab031361ef8c1755e39857d
rater31 <85116780+rater31@users.noreply.github.com>
2023-10-17 22:01:46 -0300

    fede
    Agregue un settimeout para que redirecciones a la pantalla de escritorio de alumno

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route,} from 'react-router-dom'
 import  Login from './components/Login'
 import  Loading from './components/Loading'
+import Escritorio from './components/EscritorioAlumno'
 import "./App.css";
 
 
@@ -14,6 +15,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path="/." element={<Loading/>}/>
+      <Route path="/Escritorio-Alumno" element={<Escritorio/>}/>
+
     </Routes>
     
     </>
---
+++ src/components/EscritorioAlumno.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EscritorioAlumno = () => {
+  return (
+    <div>
+        <h1>Acá va el escritorio del alumno carajo</h1>
+    </div>
+  )
+}
+
+export default EscritorioAlumno
