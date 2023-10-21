dae2ecd795d092c5888fa607d05c06adfed77cdb
rater31 <fedehidalgo89@gmail.com>
2023-10-21 10:35:32 -0300

    fe

--- src/components/EscritorioAlumno.jsx
+++ src/components/EscritorioAlumno.jsx
@@ -8,20 +8,20 @@ const EscritorioAlumno = () => {
   return (
     <div className="container-fluid">
       <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-d9">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2  min-vh-100">
+            <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none">
               <img src={LogoRojo} className='logo-rojo' alt="" srcset="" />
             </a>
             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
               <li className="nav-item">
                 <a href="#" className="nav-link align-middle px-0">
-                  <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
+                  <span className="ms-1 d-none d-sm-inline">Home</span>
                 </a>
               </li>
               <li>
                 <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                  <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
+                   <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
                 <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                   <li className="w-100">
                     <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
@@ -33,11 +33,11 @@ const EscritorioAlumno = () => {
               </li>
               <li>
                 <a href="#" className="nav-link px-0 align-middle">
-                  <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                 <span className="ms-1 d-none d-sm-inline">Orders</span></a>
               </li>
               <li>
                 <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                  <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                   <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
                 <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
                   <li className="w-100">
                     <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
@@ -49,7 +49,7 @@ const EscritorioAlumno = () => {
               </li>
               <li>
                 <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                  <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
+                 <span className="ms-1 d-none d-sm-inline">Products</span> </a>
                 <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                   <li className="w-100">
                     <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
@@ -72,9 +72,10 @@ const EscritorioAlumno = () => {
             </ul>
             <hr />
             <div className="dropdown pb-4">
-              <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
-                <span className="d-none d-sm-inline mx-1">loser</span>
+              <a href="#" className="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src={usuario} className='usuario ' alt="" srcset="" />
+
+                <span className="d-none d-sm-inline mx-1">Perfil</span>
               </a>
               <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
                 <li><a className="dropdown-item" href="#">New project...</a></li>
@@ -90,7 +91,7 @@ const EscritorioAlumno = () => {
         </div>
         <div className="col justify-content-end m-4 d-flex">
           <h3 className='me-5'>¡Bienvenido! <span className=' text-danger'>Usuario 666</span></h3>
-          <img src={usuario} className='usuario ' alt="" srcset="" />
+          {/* <img src={usuario} className='usuario ' alt="" srcset="" /> */}
         </div>
       </div>
     </div>
