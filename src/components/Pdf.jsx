cadeac9e69913192a3139a1273bc47acccb0cf76
rater31 <85116780+rater31@users.noreply.github.com>
2023-10-27 21:00:56 -0300

    fede

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import '../pdf.css'
+import PDF from '../docs/Demo.pdf'
+
+// import styled from 'styled-components'
 
 export default class Pdf extends Component {
   render() {
     return (
       <div className="container-fluid overflow-hyg">
         <div className='pdf'>
-          <object data={require('../docs/Demo.pdf')} type="aplication/pdf">
+          {/* <object data={require('../docs/Demo.pdf')} type="aplication/pdf">
 
-          </object>
+          </object> */}
+            <iframe src={PDF}  className='pdf' ></iframe>
         </div>
 
       </div>
     )
   }
 }
+
